e67dd15537617bf2f7fd8988fc13c85d00fca70f
ishika <shukla22164024@akgec.ac.in>
2023-11-16 23:50:12 +0530

    cc

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from data_loader import load_dataframe
 
 
 app = FastAPI()
+origins=["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
