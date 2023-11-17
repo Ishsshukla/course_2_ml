dabf49a6643f0619deae71281cb70c9ec7870ee1
ishika <shukla22164024@akgec.ac.in>
2023-11-17 19:53:20 +0530

    new_course _sytem

--- data_loader.py
+++
@@ -1,9 +0,0 @@
-# data_loader.py
-
-import pandas as pd
-
-def load_dataframe():
-    # Implement your DataFrame loading logic
-    # Example:
-    df = pd.read_csv('all_courses.csv')
-    return df
--- main.py
+++ main.py
@@ -1,43 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from recommendation_module import create_similarity_matrix, search_courses, recommend_courses
-from data_loader import load_dataframe
-import uvicorn
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = FastAPI()
-origins = ["http://127.0.0.1:8000/","http://localhost:5173","https://courseapi-s0hm.onrender.com/","http://localhost:5000"]
 
+# CORS (Cross-Origin Resource Sharing) settings
+origins = ["http://127.0.0.1:8000/", "http://localhost:5173", "https://courseapi-s0hm.onrender.com/", "http://localhost:5000"]
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# Load your DataFrame and create similarity matrix
+# (Note: I'm assuming you have the necessary functions in the recommendation_module and data_loader modules.)
+from recommendation_module import create_similarity_matrix
+from data_loader import load_dataframe
 
-# Load DataFrame and create a similarity matrix
 df = load_dataframe()
 similarity_matrix = create_similarity_matrix(df)
 
+# Count Vectorizer for course tags
+vectorizer = CountVectorizer()
+matrix = vectorizer.fit_transform(df['tags'].fillna(''))
 
-@app.get("/recommend/{keyword}")
-async def get_recommendations(keyword: str, top_n: int = 5):
+# Function to get recommendations based on user input
+def get_recommendations(tags, top_n=5):
+    user_vector = vectorizer.transform([tags])
+    cosine_sim_with_selected_tags = cosine_similarity(user_vector, matrix)
+
+    sim_scores = list(enumerate(cosine_sim_with_selected_tags[0]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    indices = [score[0] for score in sim_scores[:top_n]]
+    recommendations = [
+        {"courses": df['title'].iloc[i],
+         "Description": df["Description"].iloc[i],
+         "Level": df["Level"].iloc[i],
+         "Duration": df["Duration"].iloc[i],
+         "Skills Covered": df['Skills Covered'].iloc[i],
+         "prerequisites": df['prerequisites'].iloc[i],
+         "URL": df['URL'].iloc[i]}
+        for i in indices
+    ]
+    return recommendations
+
+# FastAPI endpoint for recommendations
+@app.get("/recommend/{tags}")
+async def get_recommendations_endpoint(tags: str, top_n: int = 5):
     try:
-        matching_courses = search_courses(keyword, df)
-        if not matching_courses:
-            return {"message": f"No matching courses found for {keyword}"}
-
-        recommended_courses = recommend_courses(keyword, similarity_matrix, df, top_n=top_n)
-
-        response = {"message": f"Recommendations for {keyword}", "courses": recommended_courses}
-        # response.headers["Access-Control-Allow-Origin"] = "*"
-        return response
-
+        recommendations = get_recommendations(tags, top_n)
+        return {"message": "Recommendations for tags", "courses": recommendations}
     except Exception as e:
-        return {"message": f"An error occurred during recommendation generation: {str(e)}"}
-
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+        raise HTTPException(status_code=500, detail=f"An error occurred during recommendation generation: {str(e)}")
