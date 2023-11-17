bffe13eb1e0d8fe96bfbc94190288297394c4440
ishika <shukla22164024@akgec.ac.in>
2023-11-17 10:33:08 +0530

    c10

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from recommendation_module import create_similarity_matrix, search_courses, recommend_courses
 from data_loader import load_dataframe
-
+import uvicorn
 
 
 app = FastAPI()
-origins=["*"]
+origins = ["*"]
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,25 +17,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 # Load DataFrame and create a similarity matrix
 df = load_dataframe()
 similarity_matrix = create_similarity_matrix(df)
 
+
 @app.get("/recommend/{keyword}")
 async def get_recommendations(keyword: str, top_n: int = 5):
-   
     try:
         matching_courses = search_courses(keyword, df)
         if not matching_courses:
             return {"message": f"No matching courses found for {keyword}"}
 
         recommended_courses = recommend_courses(keyword, similarity_matrix, df, top_n=top_n)
-        return {"message": f"Recommendations for {keyword}", "courses": recommended_courses}
-    
+
+        response = {"message": f"Recommendations for {keyword}", "courses": recommended_courses}
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        return response
+
     except Exception as e:
         return {"message": f"An error occurred during recommendation generation: {str(e)}"}
 
+
 # if __name__ == "__main__":
-#     import uvicorn
 #     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
